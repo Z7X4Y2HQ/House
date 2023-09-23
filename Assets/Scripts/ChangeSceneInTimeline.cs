998409c468a4414371276cd8cb02b1aacc02b799
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2023-09-24 00:46:23 +0500

    Timeline Complete... for now

---
+++ Assets/Scripts/ChangeSceneInTimeline.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneInTimeline : MonoBehaviour
+{
+     private void Update() {
+        if (HandleTimeline.currentChapter == 1 && HandleTimeline.currentScene == "Chapter_one_first_dream"){
+            SceneManager.LoadScene("House 2f");
+        }
+    }
+}
---
+++ Assets/Scripts/HandleTimeline.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleTimeline : MonoBehaviour
+{
+    public static int currentChapter = 1;
+    public static string currentScene = "Chapter_one_first_dream";
+}
