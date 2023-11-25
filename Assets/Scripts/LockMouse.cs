4b40717a1bab16435505ed067a30f9db7ada7122
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2023-11-25 11:49:05 -0600

    Update LockMouse.cs

--- Assets/Scripts/LockMouse.cs
+++ Assets/Scripts/LockMouse.cs
@@ -7,8 +7,7 @@ public class LockMouse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Lock();
     }
 
     public void Lock()
